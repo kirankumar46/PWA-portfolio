9c2eeab0c897f83d9bb8dceaa8f37a1508f0bcf0
Kiran-fed <kirankumar.r@fincelerate.com>
2025-08-12 12:58:57 +0530

    second commit

--- script.js
+++ script.js
@@ -80,6 +80,30 @@ if ('serviceWorker' in navigator) {
       .catch(err => console.log('Service Worker registration failed:', err));
   });
 }
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault(); // stop auto popup
+  deferredPrompt = e;
+
+  // Show your custom install button
+  const installBtn = document.getElementById('installBtn');
+  installBtn.style.display = 'block';
+
+  installBtn.addEventListener('click', () => {
+    installBtn.style.display = 'none';
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted install');
+      } else {
+        console.log('User dismissed install');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
 
 
 
